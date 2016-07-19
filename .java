0b517c0a70b0cd348648b17a4ce64daddf4b0487
Robin Bowes <rbowes@williamhill.co.uk>
2016-07-19 13:28:34 +0100

    Set GRADLE_HOME in .java config - again, only catering for OSX

--- .java
+++ .java
@@ -1,2 +1,5 @@
 JAVA_VERSION=1.8.0_92
 export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
+
+GRADLE_VERSION=$(gradle --version | awk '/^Gradle/ {print $2}')
+export GRADLE_HOME="/opt/boxen/homebrew/Cellar/gradle/$GRADLE_VERSION/libexec"
