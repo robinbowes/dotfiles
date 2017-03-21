41f71c4534766bc7fe6734c2001e50c0081b8eba
Robin Bowes <robin.bowes@yo61.com>
2017-03-21 13:20:55 +0000

    Handle java cleanly on Linux and OSX

--- .java
+++ .java
@@ -1,5 +1,15 @@
 JAVA_VERSION=1.8.0_121
-export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
+case "$OSTYPE" in
+  linux*)
+    export JAVA_HOME="$(dirname $(dirname $(readlink -e $(command -v javac))))"
+    ;;
+  darwin*)
+    export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
+    GRADLE_VERSION=$(gradle --version | awk '/^Gradle/ {print $2}')
+    export GRADLE_HOME="/opt/boxen/homebrew/Cellar/gradle/$GRADLE_VERSION/libexec"
+    ;;
+  *)
+    echo "Unknown OS: $OSTYPE"
+    ;;
+esac
 
-GRADLE_VERSION=$(gradle --version | awk '/^Gradle/ {print $2}')
-export GRADLE_HOME="/opt/boxen/homebrew/Cellar/gradle/$GRADLE_VERSION/libexec"
