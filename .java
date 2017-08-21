fdb7d8be45599aa1ad57ba75cd378a9af055ae97
Robin Bowes <robin.bowes@yo61.com>
2017-08-21 09:29:21 +0000

    Silence errors when java not installed on linux

--- .java
+++ .java
@@ -1,7 +1,7 @@
 JAVA_VERSION=1.8.0_121
 case "$OSTYPE" in
   linux*)
-    export JAVA_HOME="$(dirname $(dirname $(readlink -e $(command -v javac))))"
+    command -v javac && export JAVA_HOME="$(dirname $(dirname $(readlink -e $(command -v javac))))"
     ;;
   darwin*)
     export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
