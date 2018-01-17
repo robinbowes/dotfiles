2fdbc776bb5e62283c8fb28b6211f540c69c5b67
Robin Bowes <robin@TD-0437.local>
2018-01-17 01:38:11 +0000

    Only run gradle commands if gradle installed

--- .java
+++ .java
@@ -5,8 +5,10 @@ case "$OSTYPE" in
     ;;
   darwin*)
     export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
-    GRADLE_VERSION=$(gradle --version | awk '/^Gradle/ {print $2}')
-    export GRADLE_HOME="/opt/boxen/homebrew/Cellar/gradle/$GRADLE_VERSION/libexec"
+    if command -v gradle >> /dev/null ; then
+      GRADLE_VERSION=$(gradle --version | awk '/^Gradle/ {print $2}')
+      export GRADLE_HOME="/opt/boxen/homebrew/Cellar/gradle/$GRADLE_VERSION/libexec"
+    fi
     ;;
   *)
     echo "Unknown OS: $OSTYPE"
