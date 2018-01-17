dbeb5dadc499e579c2c27cb88112ca630d7ce1ca
Robin Bowes <robin.bowes@yo61.com>
2018-01-17 02:04:59 +0000

    Don't set a specific java version'

--- .java
+++ .java
@@ -1,10 +1,9 @@
-JAVA_VERSION=1.8.0_121
 case "$OSTYPE" in
   linux*)
     command -v javac && export JAVA_HOME="$(dirname $(dirname $(readlink -e $(command -v javac))))"
     ;;
   darwin*)
-    export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
+    export JAVA_HOME="$(/usr/libexec/java_home)"
     if command -v gradle >> /dev/null ; then
       GRADLE_VERSION=$(gradle --version | awk '/^Gradle/ {print $2}')
       export GRADLE_HOME="/opt/boxen/homebrew/Cellar/gradle/$GRADLE_VERSION/libexec"
